14247403bbccee52142b6c5f83c752036e8813f4
daromaj <dariuszmajewski@gmail.com>
2015-10-11 00:23:54 -0400

    experimenting with animations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function () {
     screenSetup();
 });
+var ballSize = 40;
 var screenWidth = $(window).width();
 var screenHeight = $(window).height();
 var boardSize = Math.floor(Math.min(screenHeight, screenWidth) / 3);
@@ -19,43 +20,84 @@ function animBall(dir) {
     $('.ball').stop(true, true);//stops animation in progress
     resetBall();
     var animobject = {};
+    var prop = 4;
+    var stretch = Math.floor(ballSize / prop);
+    var distance = Math.floor(boardSize / 2) - stretch;
+    var duration = 200;
     switch (dir) {
+        case "l":
+        {
+            animobject = [{
+                    "margin-left": "-=" + distance,
+                    "height": "-=" + stretch,
+                    "width": "+=" + stretch,
+                    "margin-top": "+=" + Math.floor(stretch / 2)
+                }, {
+                    "margin-left": "-=" + distance
+                }, {
+                    "height": "+=" + (stretch * 3),
+                    "width": "-=" + (stretch * 3),
+                    "margin-top": "-=" + stretch
+                }];
+            break;
+        }
         case "t":
         {
-            animobject = {
-                "margin-top": "-=" + boardSize
-            };
+            animobject = [{
+                    "margin-top": "-=" + distance,
+                    "height": "+=" + stretch,
+                    "width": "-=" + stretch
+                }, {
+                    "margin-top": "-=" + distance
+                }, {
+                    "height": "-=" + (stretch * 3),
+                    "width": "+=" + (stretch * 3)
+                }];
             break;
         }
         case "r":
         {
-            animobject = {
-                "margin-left": "+=" + boardSize
-            };
+            animobject = [{
+                    "margin-left": "+=" + distance,
+                    "height": "-=" + stretch,
+                    "width": "+=" + stretch,
+                    "margin-top": "+=" + Math.floor(stretch / 2)
+                }, {
+                    "margin-left": "+=" + distance
+                }, {
+                    "margin-left": "+=" + (stretch * 2),
+                    "height": "+=" + (stretch * 3),
+                    "width": "-=" + (stretch * 3),
+                    "margin-top": "-=" + stretch
+                }];
             break;
         }
         case "b":
         {
-            animobject = {
-                "margin-top": "+=" + boardSize
-            };
+            animobject = [{
+                    "margin-top": "+=" + distance,
+                    "height": "+=" + stretch,
+                    "width": "-=" + stretch
+                }, {
+                    "margin-top": "+=" + distance
+                }, {
+                    "margin-top": "+=" + (stretch * 2),
+                    "height": "-=" + (stretch * 3),
+                    "width": "+=" + (stretch * 3)
+                }];
             break;
         }
-        case "l":
-        {
-            animobject = {
-                "margin-left": "-=" + boardSize
-            };
-            break;
-        }
-
     }
     //apparently small animation step might help with mobile performance
-    var smallanim = $.extend({}, animobject);
-    for (var a in smallanim){
-        smallanim[a] = smallanim[a].substring(0, 2) + 1;
-    }
-    $(".ball").animate(smallanim, 10).animate(animobject, 200).promise().done(function () {
+//    var smallanim = $.extend({}, animobject[1]);
+//    for (var a in smallanim) {
+//        smallanim[a] = smallanim[a].substring(0, 2) + 1;
+//    }
+    $(".ball")
+            .animate(animobject[0], Math.floor(distance * duration / (distance * 2)), "linear")
+            .animate(animobject[1], Math.floor((distance - (stretch * 2)) * duration / (distance * 2)), "linear")
+            .animate(animobject[2], Math.floor((stretch * 8) * duration / (distance * 2)), "linear")
+            .promise().done(function () {
         resetBall();
     });
 }
